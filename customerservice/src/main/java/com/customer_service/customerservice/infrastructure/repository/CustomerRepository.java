fed7331f7d62444bc33cdc0798b7bdd322f28690
Mattheus Fernandes <mattheus.ale@hotmail.com>
2026-06-05 17:48:37 -0300

    feat: adiciona metodo da interface jpa para buscar customer pelo nome

--- customerservice/src/main/java/com/customer_service/customerservice/infrastructure/repository/CustomerRepository.java
+++ customerservice/src/main/java/com/customer_service/customerservice/infrastructure/repository/CustomerRepository.java
@@ -3,6 +3,7 @@ package com.customer_service.customerservice.infrastructure.repository;
 import com.customer_service.customerservice.infrastructure.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface CustomerRepository extends JpaRepository<Customer, UUID> {
@@ -10,4 +11,6 @@ public interface CustomerRepository extends JpaRepository<Customer, UUID> {
     boolean existsByNameIgnoreCaseAndLastnameIgnoreCase(String name, String lastname);
     boolean existsByEmail(String email);
     boolean existsByPhone(String phone);
+
+    List<Customer> findByNameContainingIgnoreCase(String name);
 }
