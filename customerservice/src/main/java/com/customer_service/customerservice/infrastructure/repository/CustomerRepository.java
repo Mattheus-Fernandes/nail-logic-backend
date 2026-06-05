86a47d344a9403e1634ff34d3783e1c644dfac3d
Mattheus Fernandes <mattheus.ale@hotmail.com>
2026-06-05 14:59:31 -0300

    feat: criado metodo derivado jpa no repository

--- customerservice/src/main/java/com/customer_service/customerservice/infrastructure/repository/CustomerRepository.java
+++ customerservice/src/main/java/com/customer_service/customerservice/infrastructure/repository/CustomerRepository.java
@@ -6,4 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface CustomerRepository extends JpaRepository<Customer, UUID> {
+
+    boolean existsByNameIgnoreCaseAndLastnameIgnoreCase(String name, String lastname);
+    boolean existsByEmail(String email);
+    boolean existsByPhone(String phone);
 }
