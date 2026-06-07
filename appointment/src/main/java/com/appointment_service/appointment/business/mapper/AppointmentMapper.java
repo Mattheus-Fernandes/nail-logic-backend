dd5108212cfbcb30de87f98190dc9307fb295f74
Mattheus Fernandes <mattheus.ale@hotmail.com>
2026-06-07 12:10:32 -0300

    feat: implementa mapper para conversor de appointment

---
+++ appointment/src/main/java/com/appointment_service/appointment/business/mapper/AppointmentMapper.java
@@ -0,0 +1,25 @@
+package com.appointment_service.appointment.business.mapper;
+
+import com.appointment_service.appointment.business.record.in.AppointmentCreateRequest;
+import com.appointment_service.appointment.business.record.out.AppointmentResponse;
+import com.appointment_service.appointment.infrastructure.entity.Appointment;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+public interface AppointmentMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "status", constant = "SCHEDULED")
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    Appointment toEntity(AppointmentCreateRequest request);
+
+    AppointmentResponse toResponse(Appointment appointment);
+
+    List<AppointmentResponse> toResponseList(List<Appointment> appointmentList);
+
+}
