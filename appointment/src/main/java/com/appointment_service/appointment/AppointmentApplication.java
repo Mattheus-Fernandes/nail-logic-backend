948dd9ef6358c0a29b44b5479966faf90932f2f9
Mattheus Fernandes <mattheus.ale@hotmail.com>
2026-06-06 22:15:13 -0300

    estrutura inicial do microsservico

---
+++ appointment/src/main/java/com/appointment_service/appointment/AppointmentApplication.java
@@ -0,0 +1,13 @@
+package com.appointment_service.appointment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointmentApplication.class, args);
+	}
+
+}
