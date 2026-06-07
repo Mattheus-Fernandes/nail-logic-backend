3a8daf77fae4e89c67a6e27419a10bcd0ebb2811
Mattheus Fernandes <mattheus.ale@hotmail.com>
2026-06-07 12:38:34 -0300

    feat: implementados metodos da interface jpa para validacao

--- appointment/src/main/java/com/appointment_service/appointment/infrastructure/repository/AppointmentRepository.java
+++ appointment/src/main/java/com/appointment_service/appointment/infrastructure/repository/AppointmentRepository.java
@@ -3,7 +3,10 @@ package com.appointment_service.appointment.infrastructure.repository;
 import com.appointment_service.appointment.infrastructure.entity.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.UUID;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, UUID> {
+    boolean existByAppointmentDateAndAppointTime(LocalDate appointmentDate, LocalTime appointmentTime);
 }
