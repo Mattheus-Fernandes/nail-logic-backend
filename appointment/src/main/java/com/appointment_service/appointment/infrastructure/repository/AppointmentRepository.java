cfe0d983d8ff67c118faa11f201d243f88d4d5d3
Mattheus Fernandes <mattheus.ale@hotmail.com>
2026-06-08 22:14:13 -0300

    implmentado metodo jpa para realizar consultas db de appointments

--- appointment/src/main/java/com/appointment_service/appointment/infrastructure/repository/AppointmentRepository.java
+++ appointment/src/main/java/com/appointment_service/appointment/infrastructure/repository/AppointmentRepository.java
@@ -1,12 +1,17 @@
 package com.appointment_service.appointment.infrastructure.repository;
 
 import com.appointment_service.appointment.infrastructure.entity.Appointment;
+import com.appointment_service.appointment.infrastructure.enums.AppointmentStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.List;
 import java.util.UUID;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, UUID> {
     boolean existsByAppointmentDateAndAppointmentTime(LocalDate appointmentDate, LocalTime appointmentTime);
+
+    List<Appointment> findByAppointmentDateBetweenAndStatus(LocalDate startDate, LocalDate endDate, AppointmentStatus status);
+    List<Appointment> findByAppointmentDateAndStatus(LocalDate appointmentDate, AppointmentStatus status);
 }
