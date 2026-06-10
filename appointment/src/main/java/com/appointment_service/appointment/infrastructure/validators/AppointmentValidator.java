b2d3ef1558076f5b7ad9b652d61492b19b20c4c8
Mattheus Fernandes <mattheus.ale@hotmail.com>
2026-06-10 14:40:08 -0300

    feat: cria metodo para validacao de atualizacao

--- appointment/src/main/java/com/appointment_service/appointment/infrastructure/validators/AppointmentValidator.java
+++ appointment/src/main/java/com/appointment_service/appointment/infrastructure/validators/AppointmentValidator.java
@@ -1,11 +1,13 @@
 package com.appointment_service.appointment.infrastructure.validators;
 
 import com.appointment_service.appointment.business.record.in.AppointmentCreateRequest;
+import com.appointment_service.appointment.infrastructure.entity.Appointment;
 import com.appointment_service.appointment.infrastructure.repository.AppointmentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Component
 @RequiredArgsConstructor
@@ -14,29 +16,43 @@ public class AppointmentValidator {
     private final AppointmentRepository appointmentRepository;
 
     public void validateCreate(AppointmentCreateRequest request) {
-        validateDate(request);
-        validateDateAndTime(request);
-        validateDateAndTime(request);
-    }
-
-
-    private void validateDate(AppointmentCreateRequest request) {
-
-        if(request.appointmentDate().isBefore(LocalDate.now())) {
-            throw new RuntimeException("Não é possível agendar para uma data passada");
-        }
-
-    }
-
-    private void validateDateAndTime(AppointmentCreateRequest request) {
+        validateDate(request.appointmentDate());
 
         boolean exist = appointmentRepository.existsByAppointmentDateAndAppointmentTime(
                 request.appointmentDate(), request.appointmentTime()
         );
 
-        if (request.appointmentDate() != null && request.appointmentTime() != null && exist) {
-            throw new RuntimeException("Horário já ocupado");
+        errorDateAndTime(exist);
+    }
+
+    public void  validateUpdate(Appointment appointment) {
+        validateDate(appointment.getAppointmentDate());
+
+        boolean exist = appointmentRepository.existsByAppointmentDateAndAppointmentTimeAndIdNot(
+                appointment.getAppointmentDate(),
+                appointment.getAppointmentTime(),
+                appointment.getId()
+        );
+
+        errorDateAndTime(exist);
+    }
+
+    private void validateDate(LocalDate date) {
+
+        if(date == null) {
+            throw new RuntimeException("A data é obrigatória");
+
         }
 
+        if(date.isBefore(LocalDate.now())) {
+            throw new RuntimeException("Não é possível agendar para uma data passada");
+        }
+    }
+
+    private void errorDateAndTime(boolean appointmentDate) {
+
+        if(appointmentDate) {
+            throw new RuntimeException("Horário já ocupado");
+        }
     }
 }
