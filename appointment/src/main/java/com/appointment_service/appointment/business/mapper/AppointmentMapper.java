cbbf258c242f9600d1404081f942124c40f44e02
Mattheus Fernandes <mattheus.ale@hotmail.com>
2026-06-11 13:13:17 -0300

    feat: adiciona conversor para builder de appointment e customer unificados

--- appointment/src/main/java/com/appointment_service/appointment/business/mapper/AppointmentMapper.java
+++ appointment/src/main/java/com/appointment_service/appointment/business/mapper/AppointmentMapper.java
@@ -2,7 +2,9 @@ package com.appointment_service.appointment.business.mapper;
 
 import com.appointment_service.appointment.business.record.in.AppointmentCreateRequest;
 import com.appointment_service.appointment.business.record.in.AppointmentUpdateRequest;
+import com.appointment_service.appointment.business.record.out.AppointmentDetailsResponse;
 import com.appointment_service.appointment.business.record.out.AppointmentResponse;
+import com.appointment_service.appointment.business.record.out.CustomerResponse;
 import com.appointment_service.appointment.infrastructure.entity.Appointment;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -23,6 +25,11 @@ public interface AppointmentMapper {
     AppointmentResponse toResponse(Appointment appointment);
 
     List<AppointmentResponse> toResponseList(List<Appointment> appointmentList);
+
+    @Mapping(target = "id", source = "appointment.id")
+    @Mapping(target = "observation", source = "appointment.observation")
+    @Mapping(target = "customer", source = "customerResponse")
+    AppointmentDetailsResponse toDetailsResponse(Appointment appointment, CustomerResponse customerResponse);
 
     void updateFromRequest(AppointmentUpdateRequest request, @MappingTarget Appointment appointment);
 }
