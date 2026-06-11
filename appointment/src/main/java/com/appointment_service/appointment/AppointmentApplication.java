1f7ce6e57c9d1ad60e6d119ee73fd5a2a3963012
Mattheus Fernandes <mattheus.ale@hotmail.com>
2026-06-11 13:10:43 -0300

    core: habilitado o feing client ao microsservico

--- appointment/src/main/java/com/appointment_service/appointment/AppointmentApplication.java
+++ appointment/src/main/java/com/appointment_service/appointment/AppointmentApplication.java
@@ -2,8 +2,10 @@ package com.appointment_service.appointment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class AppointmentApplication {
 
 	public static void main(String[] args) {
