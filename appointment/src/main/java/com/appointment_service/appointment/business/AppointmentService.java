3ca974dfc65d68ae64c1447a92e3e4e541698fe2
Mattheus Fernandes <mattheus.ale@hotmail.com>
2026-07-30 13:22:00 -0300

    feat: implementado metodo para relizar buscar de agendamento pelo id agendamento e data

--- appointment/src/main/java/com/appointment_service/appointment/business/AppointmentService.java
+++ appointment/src/main/java/com/appointment_service/appointment/business/AppointmentService.java
@@ -156,6 +156,17 @@ public class AppointmentService {
                 .toList();
     }
 
+    public AppointmentResponse findAppointmentByCustomerIdAndDate(String phone) {
+
+        LocalDate tomorrow = LocalDate.now().plusDays(1);
+
+        CustomerResponse customer =  customerClient.findByPhone(phone);
+
+        Appointment appointment = appointmentRepository.findByCustomerIdAndAppointmentDate(customer.id(), tomorrow);
+
+        return appointmentMapper.toResponse(appointment);
+    }
+
     public AppointmentDetailsResponse updateAppointmentStatus(UUID id, AppointmentStatus status) {
 
         Appointment appointment = appointmentRepository.findById(id).orElseThrow(
@@ -165,6 +176,8 @@ public class AppointmentService {
         CustomerResponse customer = customerClient.findById(appointment.getCustomerId());
 
         appointment.setStatus(status);
+
+        appointmentRepository.save(appointment);
 
         return appointmentMapper.toDetailsResponse(appointment, customer);
     }
@@ -182,6 +195,8 @@ public class AppointmentService {
         appointmentValidator.validateUpdate(appointment);
 
         appointment.setUpdatedAt(LocalDateTime.now());
+
+        appointmentRepository.save(appointment);
 
         return appointmentMapper.toDetailsResponse(appointment, customer);
 
