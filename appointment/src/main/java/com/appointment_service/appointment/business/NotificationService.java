318cfe8170a033b71720dd2c295fa90a44ba86de
Mattheus Fernandes <mattheus.ale@hotmail.com>
2026-07-30 10:34:43 -0300

    feat: implementado integracao com webbook n8n

--- appointment/src/main/java/com/appointment_service/appointment/business/NotificationService.java
+++ appointment/src/main/java/com/appointment_service/appointment/business/NotificationService.java
@@ -24,7 +24,6 @@ public class NotificationService {
             String serviceName
     ){
         Map<String, Object> body = new HashMap<>();
-
         body.put("customerName", customerName);
         body.put("customerLastname", customerLastname);
         body.put("phone", phone);
