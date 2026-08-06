588c5eb17794961b5a9ca7ab41041f13daeba6e5
Mattheus Fernandes <mattheus.ale@hotmail.com>
2026-08-06 10:11:32 -0300

    feat: habilitado CORS

--- customerservice/src/main/java/com/customer_service/customerservice/controller/CustomerController.java
+++ customerservice/src/main/java/com/customer_service/customerservice/controller/CustomerController.java
@@ -15,6 +15,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/customers")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class CustomerController {
 
     private final CustomerService customerService;
